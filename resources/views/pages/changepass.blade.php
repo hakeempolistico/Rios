4e0d8df5545e4e002f9f06eb8df5c7563a046d10
hakeempolistico <hjpolistico@gmail.com>
2018-08-08 19:26:22 +0800

    added card in chagepass

--- resources/views/pages/changepass.blade.php
+++ resources/views/pages/changepass.blade.php
@@ -20,35 +20,44 @@
   @include('includes.messages')
 
   <div class="row">
-    
-      <div class="col-md-12">         
-      <form id="form-change-password" role="form" method="POST" action="{{ url('/changepass/credentials') }}" novalidate class="form-horizontal">    
-        <label for="current-password" class="col-sm-4 control-label">Current Password</label>
-        <div class="col-sm-12">
-          <div class="form-group">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
-            <input type="password" class="form-control" id="current-password" name="current-password" placeholder="Password">
-          </div>
-        </div>
-        <label for="password" class="col-sm-4 control-label">New Password</label>
-        <div class="col-sm-12">
-          <div class="form-group">
-            <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-          </div>
-        </div>
-        <label for="password_confirmation" class="col-sm-4 control-label">Re-enter Password</label>
-        <div class="col-sm-12">
-          <div class="form-group">
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Re-enter Password">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-12">
-            <button type="submit" class="btn btn-danger">Submit</button>
-          </div>
-        </div>
+      <div class="col-md-12">      
+      <div class="card ">
+              <div class="card-header ">
+                <h5 class="card-title">Change Password</h5>
+                <p class="card-category">Input Credentials</p>
+              </div>
+              <div class="card-body ">
+                <form id="form-change-password" role="form" method="POST" action="{{ url('/changepass/credentials') }}" novalidate class="form-horizontal">    
+                  <label for="current-password" class="col-sm-4 control-label">Current Password</label>
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+                      <input type="password" class="form-control" id="current-password" name="current-password" placeholder="Password">
+                    </div>
+                  </div>
+                  <label for="password" class="col-sm-4 control-label">New Password</label>
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                    </div>
+                  </div>
+                  <label for="password_confirmation" class="col-sm-4 control-label">Re-enter Password</label>
+                  <div class="col-sm-12">
+                    <div class="form-group">
+                      <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" placeholder="Re-enter Password">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-sm-12">
+                      <button type="submit" class="btn btn-danger">Submit</button>
+                    </div>
+                  </div>
+              </form>
+              </div>
+              <div class="card-footer ">
+              </div>
+            </div>   
       </div>
-    </form>
   </div>
 </div>
 
